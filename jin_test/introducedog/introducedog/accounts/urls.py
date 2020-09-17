af8e0b74c7c29febefa49b3de5f8ae02cbbb5f34
jin <choi_jin@kakao.com>
2020-09-17 17:21:33 +0900

    BE | bcrypt, jwt | S03P22A307-60, S03P22A307-61

--- jin_test/introducedog/introducedog/accounts/urls.py
+++ jin_test/introducedog/introducedog/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignupView, LoginView, LogoutView, MypageView
+from .views import SignupView, LoginView, LogoutView, MypageView, ApicheckView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/login', LoginView.as_view()),
     path('/logout', LogoutView.as_view()),
-    path('', MypageView.as_view()),
+    path('/mypage', MypageView.as_view()),
+    path('/islogin', ApicheckView.as_view()),
 ]
--- jin_test/introducedog/introducedog/accounts/views.py
+++ jin_test/introducedog/introducedog/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 import json
+import bcrypt
+import jwt
+
+from introducedog.settings import SECRET_KEY
 
 from django.views import View
 from django.http import HttpResponse, JsonResponse
@@ -16,11 +19,17 @@ class SignupView(View):
             if User.objects.filter(user_email = data['user_email']).exists(): # 존재하는 이메일인지 확인
                 return JsonResponse({"message" : "이미 존재하는 이메일 입니다."}, status = 400)
             
+            #비밀번호 암호화 부분!
+            user_password = data['user_password'].encode('utf-8') #바이트 코드로 변환
+            password_crypt = bcrypt.hashpw(user_password, bcrypt.gensalt()) #암호화
+            password_crypt = password_crypt.decode('utf-8') #다시 utf-8로 변환
+
+
             User(
                 # user_id = data['user_id'],
                 user_name = data['user_name'],
                 user_email = data['user_email'],
-                user_password = data['user_password'],
+                user_password = password_crypt,    #암호화된 패스워드 디비에 저장하자!
                 user_profile = data['user_profile'],
                 match_dog = data['match_dog'],
                 same_dog = data['same_dog'],
@@ -43,21 +52,42 @@ class LoginView(View):
         #     user_email = data['user_email'],
         #     user_password = data['user_password']
         # )
-        if User.objects.filter(user_email = data['user_email']).exists() :
-            user = User.objects.get(user_email = data['user_email'])
-            # print(user.user_name)
-            if user.user_password == data['user_password'] :
-                request.session['username'] = user.user_name
-                # print(request.session.get('username'))
-                return JsonResponse({'message':f'{user.user_name}님 로그인 성공!'}, status=200)
-            else :
-                return JsonResponse({'message':'비밀번호가 틀렸어요'},status = 200)
+        try:
+            if User.objects.filter(user_email = data['user_email']).exists() :
+                user = User.objects.get(user_email = data['user_email'])
 
-        return JsonResponse({'message':'등록되지 않은 이메일 입니다.'},status=200)
+                if bcrypt.checkpw(data['user_password'].encode('utf-8'), user.user_password.encode('utf-8')):
+                    token = jwt.encode({'user_email' : data['user_email']}, SECRET_KEY, algorithm = "HS256")
+                    token = token.decode('utf-8')                          # 유니코드 문자열로 디코딩
+                    request.session['username'] = user.user_name
+                    print(request.session.get('username'))
+                    return JsonResponse({"token" : token }, status=200)
+                else:
+                    return HttpResponse(status = 401)
 
-    def get(self, request):
-        user = User.objects.values()
-        return JsonResponse({"list" : list(user)}, status = 200)
+            return HttpResponse(status=400)
+        
+        except KeyError:
+            return JsonResponse({"message":"INVALID_KEYS"}, status = 400)
+            # if user.user_password == data['user_password'] :
+            #     request.session['username'] = user.user_name
+            #     # print(request.session.get('username'))
+            #     return JsonResponse({'message':f'{user.user_name}님 로그인 성공!'}, status=200)
+            # else :
+            #     return JsonResponse({'message':'비밀번호가 틀렸어요'},status = 200)
+
+        # return JsonResponse({'message':'등록되지 않은 이메일 입니다.'},status=200)
+
+class TokenCheckView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+
+        user_token_info = jwt.decode(data['token'], SECRET_KEY, algorithm = 'HS256')
+
+        if User.objects.filter(user_email=user_token_info['user_email']).exists() :
+            return HttpResponse(status=200)
+
+        return HttpResponse(status=403)
     
 class LogoutView(View):
     def post(self, request):
@@ -72,3 +102,8 @@ class MypageView(View) :
         myuser = User.objects.filter(user_name = request.session.get('username')).values()
         return JsonResponse({'user':list(myuser)}, status = 200)
 
+class ApicheckView(View):
+    def get(self, request):
+        if request.session['username'] :
+            return JsonResponse({'message': 'true'})
+    
