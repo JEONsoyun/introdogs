e0364e9156cd176baa1f0f7ddfc8387e9f06aee5
jin <choi_jin@kakao.com>
2020-09-18 11:13:57 +0900

    create jin branch

--- backend/introducedog/introducedog/likes/models.py
+++ backend/introducedog/introducedog/likes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Like(models.Model):
+    user_id = models.IntegerField(null=True)
+    dog_id = models.IntegerField(null=True)
+    # user_id = models.AutoField(primary_key=True) #사용자 id
+    # user_name = models.CharField(max_length=128, null=True) #사용자 이름
+    # user_email = models.CharField(max_length=128, null=True) #사용자 email
+    # user_password = models.CharField(max_length=128, null=True) #사용자 password
+    # user_profile = models.CharField(max_length=300, null=True) #사용자 프로필 사진
+    # match_dog = models.IntegerField( null=True) #사용자와 잘 어울리는 강아지 id
+    # same_dog = models.CharField(max_length=300, null=True) #사용자와 닮은 강아지 사진
+    
