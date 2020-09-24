711ea2f3f776fcaabe11b6123aa90deeadf12b1f
chaeeun kim <fairy037@naver.com>
2020-09-24 14:49:58 +0900

    잃어버린 멍멍이 찾기 결과 화면 퍼블리싱 완료

---
+++ frontend/src/components/common/s-dog-profile-small.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="s-dog-profile-small">
+    <div class="d-flex s-dog-profile-small-box">
+      <div class="s-dog-profile-small-image" :style="`background-image: url(${data.thumnail})`">
+        <div class="s-dog-profile-small-heart">
+          <v-icon color="red">favorite</v-icon>
+        </div>
+      </div>
+      <div class="s-dog-profile-small-content">
+        <div style="margin-bottom: 4px;">{{data.dog_id}}</div>
+        <div class="d-flex s-dog-profile-small-row">
+          <div class="s-dog-profile-small-title">나이</div>
+          <div class="s-dog-profile-small-text">{{data.age}}</div>
+        </div>
+        <div class="s-dog-profile-small-bar" />
+        <div class="d-flex s-dog-profile-small-row">
+          <div class="s-dog-profile-small-title">성별</div>
+          <div class="s-dog-profile-small-text">{{data.sex == 'M' ? '남' : '여'}}</div>
+        </div>
+        <div class="s-dog-profile-small-bar" />
+        <div class="d-flex s-dog-profile-small-row">
+          <div class="s-dog-profile-small-title">견종</div>
+          <div class="s-dog-profile-small-text">{{data.kind}}</div>
+        </div>
+        <div class="s-dog-profile-small-bar" />
+        <div class="d-flex s-dog-profile-small-row">
+          <div class="s-dog-profile-small-title">털색</div>
+          <div class="s-dog-profile-small-text">{{data.color}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 's-dog-profile-small',
+  props: {
+    data: { type: Object, default: () => ({}) },
+  },
+  data: () => ({}),
+  methods: {},
+};
+</script>
+
+<style>
+.s-dog-profile-small-box {
+  width: 100%;
+  padding: 16px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  border: solid 1px #ffd501;
+  background-color: #ffffff;
+  font-size: 12px;
+  font-weight: 700;
+  letter-spacing: -0.48px;
+  color: #1e1e1e;
+}
+
+.s-dog-profile-small-image {
+  position: relative;
+  margin-right: 16px;
+  height: 150px;
+  width: 150px;
+  background-size: cover;
+  background-position: center center;
+}
+
+.s-dog-profile-small-heart {
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: rgba(255, 255, 255, 0.61);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.s-dog-profile-small-row {
+  width: 100%;
+  padding: 6px 0;
+  display: flex;
+  align-items: center;
+}
+
+.s-dog-profile-small-title {
+  color: #ffd501;
+  font-size: 12px;
+  font-weight: 700;
+  letter-spacing: -0.6px;
+  margin-right: 10px;
+  flex-shrink: 0;
+}
+
+.s-dog-profile-small-text {
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: -0.48px;
+  color: #616161;
+}
+
+.s-dog-profile-small-bar {
+  width: 100%;
+  border-bottom: solid 1px #eee;
+}
+
+.s-dog-profile-small-empty-image {
+  margin-top: 24px;
+  width: 220px;
+  opacity: 0.4;
+}
+
+.s-dog-profile-small-empty-text {
+  margin: 32px;
+  font-size: 16px;
+  color: #585858;
+}
+</style>
---
+++ frontend/src/pages/lost/result.vue
@@ -0,0 +1,65 @@
+<template>
+  <s-first-layout title="잃어버린 멍멍이 찾기 결과">
+    <div class="lost-result-page">
+      <div class="lost-result-page-map"></div>
+      <div class="lost-result-page-list"></div>
+      <s-dog-profile-small style="margin-top: 24px;" :data="dog" />
+      <s-button
+        @click="onDetailClick"
+        style="border-top-left-radius:0;border-top-right-radius:0;"
+        size="small"
+      >자세히 보기</s-button>
+    </div>
+  </s-first-layout>
+</template>
+
+<script>
+export default {
+  name: 'lost-result-page',
+  data: () => ({
+    dog: {
+      dog_id: 'N448548202000333',
+      age: '2018(년생)',
+      weight: '10(Kg)',
+      sex: 'M',
+      kind: '진도견',
+      color: '흰색',
+      neuter: 'N',
+      thumnail:
+        'http://www.animal.go.kr/files/shelter/2020/07/202009151909319_s.jpg',
+      profile:
+        'http://www.animal.go.kr/files/shelter/2020/07/202009151909319.jpg',
+      careAddr:
+        '경상남도 합천군 합천읍 옥산로 16 (합천읍/ 까치빌라) 태민동물병원',
+      careNm: '태민동물병원',
+      special: '겁이 많고 경계심이 많아서 조심성이 많은 성격',
+      find_place: '합천읍 충효로',
+      find_date: '20200915',
+      end_date: '20200925',
+    },
+  }),
+  methods: {
+    onDetailClick() {
+      this.$router.push(`/detail/${this.dog.dog_id}`);
+    },
+  },
+};
+</script>
+
+<style>
+.lost-result-page {
+  padding: 0 16px;
+  padding-top: 64px;
+  padding-bottom: 40px;
+}
+
+.lost-result-page-map {
+  height: 160px;
+  background: #eee;
+}
+
+.lost-result-page-list {
+  height: 160px;
+  background: #fff;
+}
+</style>
