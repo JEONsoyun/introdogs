54ee3ff680322c8e7a3d09c16842f044695aafcb
jin <choi_jin@kakao.com>
2020-09-25 19:33:04 +0900

    shelter

--- backend/introducedog/introducedog/arrounds/tests.py
+++ backend/introducedog/introducedog/arrounds/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from geopy import distance
+
+from haversine import haversine
+
+def get_distance():
+    st = (float(37.8701158122), float(126.9835430508))
+    end = (float(37.8459716369), float(127.4986540628))
+    print("sksk")
+    print(haversine(st, end))
