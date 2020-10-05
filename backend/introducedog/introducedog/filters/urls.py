d6b14d0d261a146df4264503e011326d6aa9e4fe
Seunghee Suh <pucca94@naver.com>
2020-10-05 13:39:28 +0900

    update filter url

--- backend/introducedog/introducedog/filters/urls.py
+++ backend/introducedog/introducedog/filters/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from filters.views import DogColorMatch, DogColorContainMatch
 
 urlpatterns = [
-    path('', DogColorMatch.as_view()),
-    path('two/', DogColorContainMatch.as_view())
+    #path('pre/', DogColorMatch.as_view()),
+    path('', DogColorContainMatch.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
